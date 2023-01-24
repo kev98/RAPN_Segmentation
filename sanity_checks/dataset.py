022e419c1ff25edc7d6e6d1bddf650492bdad509
kev98 <kevinmarchesini@gmail.com>
2023-01-24 11:04:01 +0100

    Code for the sanity check of the dataset

---
+++ sanity_checks/dataset.py
@@ -0,0 +1,163 @@
+import glob
+from torch.utils.data import Dataset as BaseDataset
+import cv2
+# to avoid multiprocessing deadlocks
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+import numpy as np
+from utils.semantic_masks import binary_mask
+import json
+
+
+class RAPN_Dataset(BaseDataset):
+    """SAR-RARP50 Dataset. Read images and corresponding masks, apply augmentation and preprocessing transformations.
+
+    Args:
+        images_dir (str): path to images folder
+        classes (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transformation pipeline (e.g. flip, scale etc.)
+        preprocessing (albumentations.Compose): data preprocessing (e.g. normalization, shape manipulation, etc.)
+    """
+
+    def __init__(
+            self,
+            images_dir,
+            classes,
+            augmentation=None,
+            preprocessing=None,
+    ):
+
+        self.images = glob.glob(images_dir + '/raw_images/*/*.png')
+        self.masks = glob.glob(images_dir + '/masks/*/*.png')
+        self.images.sort()
+        self.masks.sort()
+        print('number of images: ',len(self.images))
+        print('number of masks: ', len(self.masks))
+
+        # convert str names to class values on masks, using the "config.json" file
+        self.classes = classes
+        with open("../config_files/config.json", 'r') as openfile:
+            # Reading from json file
+            json_file = json.load(openfile)
+        mapping = json_file["labels"]
+        self.class_values = []
+        self.other_label = json_file['classes'] + 1
+        if len(classes) == 2:
+            self.class_values.append(0)
+            self.class_values.append(1)
+        else:
+            for cls in classes:
+                if cls == 'Other instruments':
+                    self.class_values.append(self.other_label)
+                    continue
+                if cls == 'Tissue' or cls == 'Background':
+                    self.class_values.append(0)
+                    continue
+                if cls not in mapping.keys():
+                    cls = cls + '_1'
+                if cls not in mapping.keys():
+                    cls = cls.replace('_1', '_01')
+                try:
+                    self.class_values.append(mapping[cls]["label"])
+                except KeyError as e:
+                    print('I got a KeyError - Class not valid: %s'%str(e))
+        print('class values: ', self.class_values)
+
+        # List of instruments to be annotated as "Other instruments" (not always necessary)
+        self.other_instruments = []
+        # N.B. in any case "Other instruments" class must be the last one of the self.classes list
+        if self.classes[-1] == 'Other instruments':
+            for i in range(1, self.other_label):
+                if i not in self.class_values:
+                    self.other_instruments.append(i)
+
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+
+    def __getitem__(self, i):
+        # read image and mask
+        #print('image: ', self.images[i])
+        rel_path = self.images[i].split('/')[-1]
+        procedure = rel_path[:-9]
+        n_image = rel_path[-8:-4]
+        image = cv2.imread(self.images[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks[i], 0)
+
+        # if we need the class "Other instruments"
+        # N.B. in any case "Other instruments" class must be the last one of the self.classes list
+        if self.classes[-1] == 'Other instruments':
+            for oth in self.other_instruments:
+                mask[mask == oth] = self.other_label
+
+        # if we want to create a dataset for binary segmentation
+        if len(self.classes) == 2:
+            # set the "Inside body" class as background
+            mask[mask == 15] = 0
+            mask = binary_mask(mask)
+            # separate the masks of the different classes and stack them
+            masks = [(mask == v) for v in self.class_values]
+            mask = np.stack(masks, axis=-1).astype('float')
+        # if we want to create a dataset for multiclass segmentation
+        else:
+            # separate the masks of the different classes and stack them
+            masks = [(mask == v) for v in self.class_values]
+            mask = np.stack(masks, axis=-1).astype('float')
+            sum = np.sum(mask, axis=2)
+            result = np.logical_not(np.logical_xor(sum, mask[:, :, 0])).astype('float')
+            mask[:, :, 0] = result
+
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        return image, mask, procedure, n_image
+
+    def __len__(self):
+        return len(self.images)
+
+
+# Code to try the dataloader
+
+'''classes = ['Background', 'Instrument']
+classes = ['Tissue', 'Force Bipolar', 'Fenestrated Bipolar Forceps', 'Prograsp Forceps', 'Monopolar Curved Scissors',
+           'Suction', 'Large Needle Driver', 'Echography', 'Inside Body']
+classes = ['Tissue', 'Monopolar Curved Scissors', 'Force Bipolar', 'Large Needle Driver', 'Suction',
+           'Suture wire', 'Hemolock Clip', 'Fenestrated Bipolar Forceps', 'Suture needle', 'Prograsp Forceps',
+           'Vessel Loop', 'Cadiere Forceps', 'Gauze', 'Bulldog clamp', 'Da Vinci trocar', 'Echography',
+           'Laparoscopic Fenestrated Forceps', 'Bulldog wire', 'Endobag', 'Veriset', 'Hemolock Clip Applier',
+           'Laparoscopic Needle Driver']
+classes = ['Tissue', 'Monopolar Curved Scissors', 'Force Bipolar', 'Large Needle Driver', 'Suction',
+           'Suture wire', 'Hemolock Clip', 'Fenestrated Bipolar Forceps', 'Suture needle', 'Prograsp Forceps',
+           'Vessel Loop', 'Cadiere Forceps', 'Gauze', 'Bulldog clamp', 'Da Vinci trocar', 'Echography',
+           'Laparoscopic Fenestrated Forceps', 'Bulldog wire', 'Endobag', 'Veriset', 'Hemolock Clip Applier',
+           'Laparoscopic Needle Driver', 'Other instruments']
+dataset = RAPN_Dataset(r"/Volumes/ORSI/Kevin/Dataset_RAPN_20procedures/train", classes)
+for i in range(38):
+    image, mask, p, n = dataset.__getitem__(i)
+#image, mask, p, n = dataset.__getitem__(10)
+print(image.shape, mask.shape)
+for i in range(len(classes)):
+    print(i, np.count_nonzero(mask[:, :, i] == 1))
+'''
+
+# Code to understand the conversion of the classes
+'''mask = np.array([[0,1,2], [1,4,5], [2,3,6]])
+class_values = [0, 1, 3, 4]
+
+masks = [(mask == v) for v in class_values]
+print(masks[0].shape)
+mask = np.stack(masks, axis=-1).astype('float')
+sum = np.sum(mask, axis=2)
+print('sum', sum)
+result = np.logical_not(np.logical_xor(sum, mask[:, :, 0])).astype('float')
+print(result)
+print('mask before',mask)
+mask[:, :, 0] = result
+print('mask after', mask)'''
---
+++ sanity_checks/sanity_check_classes.py
@@ -0,0 +1,175 @@
+import os
+import argparse
+
+import numpy as np
+import torch
+import pandas as pd
+import segmentation_models_pytorch as smp
+from dataset import RAPN_Dataset
+from torch.utils.data import DataLoader
+from utils.albumentation import get_training_augmentation, get_preprocessing, get_validation_augmentation
+import tqdm
+
+# Parse input argument
+parser = argparse.ArgumentParser(description="Network for segmentation in RAPN",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-a", "--activation", help="activation function", default="softmax2d")
+parser.add_argument("encoder", help="Encoder of the network")
+parser.add_argument("model", help="Architecture model of the network (FPN, DeepLabV3+, U-Net...)")
+args = parser.parse_args()
+config = vars(args)
+
+# Set the hyperparameters of the network
+ACTIVATION = config['activation']
+LEARNING_RATE = 3e-4
+PATIENCE = 15
+
+# Definition of the segmentation classes
+#classes = ["Background", "Instrument"]
+classes = ['Tissue', 'Force Bipolar', 'Fenestrated Bipolar Forceps', 'Prograsp Forceps', 'Monopolar Curved Scissors',
+           'Suction', 'Large Needle Driver', 'Echography']
+iou_th = [0.85, 0.7, 0.7, 0, 0.75, 0.7, 0.6, 0.7]
+#classes = ['Tissue', 'Monopolar Curved Scissors', 'Force Bipolar', 'Large Needle Driver', 'Suction',
+#           'Suture wire', 'Hemolock Clip', 'Fenestrated Bipolar Forceps', 'Suture needle', 'Prograsp Forceps',
+#           'Vessel Loop', 'Cadiere Forceps', 'Gauze', 'Bulldog clamp', 'Da Vinci trocar', 'Echography',
+#           'Laparoscopic Fenestrated Forceps', 'Bulldog wire', 'Endobag', 'Veriset', 'Hemolock Clip Applier',
+#           'Laparoscopic Needle Driver']
+#classes = ['Tissue', 'Monopolar Curved Scissors', 'Force Bipolar', 'Large Needle Driver', 'Suction',
+#           'Suture wire', 'Hemolock Clip', 'Fenestrated Bipolar Forceps', 'Suture needle', 'Prograsp Forceps',
+#           'Vessel Loop', 'Cadiere Forceps', 'Gauze', 'Bulldog clamp', 'Da Vinci trocar', 'Echography',
+#           'Laparoscopic Fenestrated Forceps', 'Bulldog wire', 'Endobag', 'Veriset', 'Hemolock Clip Applier',
+#           'Laparoscopic Needle Driver', 'Other instruments']
+#classes = ['Tissue', 'Monopolar Curved Scissors', 'Force Bipolar', 'Large Needle Driver', 'Suction',
+#           'Suture wire', 'Hemolock Clip', 'Fenestrated Bipolar Forceps', 'Suture needle', 'Prograsp Forceps',
+#           'Vessel Loop', 'Cadiere Forceps', 'Gauze', 'Bulldog clamp', 'Da Vinci trocar', 'Echography',
+#           'Laparoscopic Fenestrated Forceps', 'Bulldog wire', 'Endobag', 'Veriset', 'Hemolock Clip Applier',
+#           'Laparoscopic Needle Driver', 'Airseal trocar', 'Endobag wire', 'Endobag specimen retriever',
+#           'Laparoscopic Clip Applier', 'Drain', 'Metal clip', 'Laparoscopic Scissors', 'Foam extruder',
+#           'Assistant trocar', 'Fibrilar', 'Left PBP Needle Driver']
+#classes = ['Tissue', 'Monopolar Curved Scissors', 'Hemolock Clip', 'Fenestrated Bipolar Forceps', 'Suction', 'Large Needle Driver', 'Suture wire',
+#                    'Vessel Loop', 'Suture needle', 'Bulldog clamp', 'Echography', 'Laparoscopic Clip Applier', 'Gauze', 'Endobag']
+
+#Choose the encoder and the segmentation model
+ENCODER = config['encoder']  # encoder
+ENCODER_WEIGHTS = 'imagenet'  # pretrained weights
+CLASSES = classes
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+MODEL_NAME = config['model']  # segmentation model
+
+# DATA ROOT
+DATA_DIR = r"/home/kmarc/workspace/nas_private/Segmentation_Dataset_RAPN/test"
+dest = "/home/kmarc/workspace/nas_private/sanity_check_output"
+#DATA_DIR = "/Volumes/ORSI/Kevin/Dataset_RAPN_20procedures/test"
+#dest = "/Volumes/ORSI/Kevin/Dataset_RAPN_20procedures"
+
+
+def computeIoU(prediction, target, num_classes):
+    IoU = []
+    for index in range(num_classes):
+        targ = np.reshape(np.array(prediction) == index, (1, -1))
+        pred = np.reshape(np.array(target) == index, (1, -1))
+        intersection = np.sum(np.bitwise_and(targ, pred))
+        union = np.sum(np.bitwise_or(targ, pred))
+        iou_class= (intersection/(union + 1e-10))
+        IoU.append(iou_class)
+
+    return IoU
+
+
+def main():
+
+    # create segmentation model with pretrained encoder
+    if MODEL_NAME == 'FPN':
+        model = smp.FPN(
+            encoder_name=ENCODER,
+            encoder_weights=ENCODER_WEIGHTS,
+            classes=len(classes),
+            activation=ACTIVATION,
+            aux_params={'classes': len(classes), 'dropout': 0.38}
+        )
+    elif MODEL_NAME == 'DeepLabV3+':
+        model = smp.DeepLabV3Plus(
+            encoder_name=ENCODER,
+            encoder_weights=ENCODER_WEIGHTS,
+            encoder_output_stride=16,
+            classes=len(classes),
+            activation=ACTIVATION,
+            aux_params={'classes': len(classes), 'dropout': 0.38}
+        )
+    elif MODEL_NAME == 'Unet++':
+        model = smp.UnetPlusPlus(
+            encoder_name=ENCODER,
+            encoder_weights=ENCODER_WEIGHTS,
+            decoder_channels=[256, 128, 64, 32, 16],
+            classes=len(classes),
+            activation=ACTIVATION,
+            aux_params={'classes': len(classes), 'dropout': 0.38}
+        )
+    else:
+        model = None
+
+    # define preprocessing function
+    #preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+    preprocessing_fn = smp.encoders.get_preprocessing_fn('timm-mobilenetv3_large_100', ENCODER_WEIGHTS)
+    model.load_state_dict(torch.load("/home/kmarc/workspace/nas_private/RAPN_results/base_model/binary/FPNtu-efficientnetv2_rw_s_bs16_lr0.0002_focaldice/tu-efficientnetv2_rw_s-FPN-30ce.pth"))
+    #model.load_state_dict(torch.load("/Users/kevinmarchesini/Desktop/tu-efficientnet_b4-DeepLabV3+-30ce.pth", map_location='cpu'))
+    model.to(DEVICE)
+
+    # TEST SET
+    test_dataset = RAPN_Dataset(
+        DATA_DIR,
+        classes=CLASSES,
+        augmentation=get_validation_augmentation(),
+        preprocessing=get_preprocessing(preprocessing_fn),
+    )
+
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    #print(train_dataset.__getitem__(0)[1].shape)
+
+    dir_proc = os.listdir(os.path.join(DATA_DIR, 'raw_images'))
+    dict_proc = {}
+    print(dir_proc)
+    for d in dir_proc:
+        if '._' not in d:
+            dict_proc[d] = []
+
+    print(dict_proc)
+
+    #for image, mask, p, num in test_loader:
+    for sample in tqdm.tqdm(test_loader):
+        image, mask, p, num = sample
+        p, num = p[0], num[0]
+        mask = np.argmax(mask[0], axis=0).cpu().numpy()
+
+        if DEVICE == 'cuda':
+            image = image.to(device=torch.device('cuda'))
+
+        pred = np.argmax(model.predict(image)[0].cpu().squeeze(), axis=0)
+
+        IoU = computeIoU(pred, mask, len(classes))
+        print(p, num, IoU)
+
+        # if the IoU of a class present in the real mask is low than the threshold we add the image to the list
+        str = ""
+        for i in range(len(classes)):
+            if np.any(mask == i):
+                if IoU[i] < iou_th[i]:
+                    str = str + " " + classes[i]
+        if str != "":
+            dict_proc[p].append(num + str)
+
+    for k in dict_proc.keys():
+        dict_proc[k] = pd.Series(dict_proc[k])
+
+    # create a Dataframe with the occurencies of each class
+    df = pd.DataFrame.from_dict(dict_proc)
+    print(df)
+
+    df.to_excel(dest + '/check_8classes_test.xlsx')
+
+
+if __name__ == '__main__':
+
+    torch.cuda.empty_cache()
+    main()
