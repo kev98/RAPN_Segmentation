c76d58e87bf6b4d0926063bb478012cb1e509625
kev98 <kevinmarchesini@gmail.com>
2023-02-14 15:11:08 +0100

    Remove the HUD from the bottom of the images

--- dataset/remove_hud.py
+++ dataset/remove_hud.py
@@ -28,28 +28,39 @@ def main():
     source_folder = r"/home/kmarc/workspace/nas_private/Segmentation_Dataset_RAPN_2/train"
     #source_folder = r"/Volumes/ORSI/Kevin/Segmentation_Dataset_RAPN_2/train"
 
-    images = glob.glob(source_folder + '/raw_images/*/*.png')
-    masks = glob.glob(source_folder + '/masks/*/*.png')
-    images.sort()
-    masks.sort()
+    procedures = ['RAPN7', 'RAPN8', 'RAPN10', 'RAPN11', 'RAPN12', 'RAPN33', 'RAPN34', 'RAPN35',
+                  'RAPN36', 'RAPN37', 'RAPN38', 'RAPN39', 'RAPN41', 'RAPN54', 'RAPN61', 'RAPN62',
+                  'RAPN76', 'RAPN77', 'RAPN78', 'RAPN79', 'RAPN81', 'RAPN82', 'RAPN85', 'RAPN86',
+                  'RAPN87', 'RAPN88', 'RAPN89', 'RAPN90', 'RAPN92', 'RAPN93', 'RAPN94', 'RAPN95',
+                  'RAPN96', 'RAPN97', 'RAPN98', 'RAPN99', 'RAPN100', 'RAPN102', 'RAPN103', 'RAPN104',
+                  'RAPN107', 'RAPN108', 'RAPN109', 'RAPN110', 'RAPN111', 'RAPN114', 'RAPN115', 'RAPN116',
+                  'RAPN117', 'RAPN118', 'RAPN119', 'RAPN120', 'RAPN121', 'RAPN122']
 
-    with alive_bar(len(images)) as bar:
-        for im_path, mask_path in zip(images, masks):
-            # open the image and the mask and retrieve the size
-            im = cv2.cvtColor(cv2.imread(im_path), cv2.COLOR_BGR2RGB)
-            mask = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2RGB)
-            width, height = mask.shape[:-1]
+    for p in procedures:
+        images = glob.glob(source_folder + '/raw_images/' + p + '/*.png')
+        masks = glob.glob(source_folder + '/masks/' + p + '/*.png')
+        images.sort()
+        masks.sort()
 
-            # check if the image has to be cropped
-            if width > 900 and height > 900:
-                # crop the image and the mask and save them
-                im_cropped, mask_cropped = crop(im, mask)
-                print(im.shape, im_cropped.shape)
-                print(mask.shape, mask_cropped.shape)
-                cv2.imwrite(im_path, cv2.cvtColor(im_cropped, cv2.COLOR_BGR2RGB))
-                cv2.imwrite(mask_path, cv2.cvtColor(mask_cropped, cv2.COLOR_BGR2RGB))
-            # Update the progress bar
-            bar()
+        if len(images) > 0:
+            print(p)
+            with alive_bar(len(images)) as bar:
+                for im_path, mask_path in zip(images, masks):
+                    # open the image and the mask and retrieve the size
+                    im = cv2.cvtColor(cv2.imread(im_path), cv2.COLOR_BGR2RGB)
+                    mask = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2RGB)
+                    width, height = mask.shape[:-1]
+
+                    # check if the image has to be cropped
+                    if width > 900 and height > 900:
+                        # crop the image and the mask and save them
+                        im_cropped, mask_cropped = crop(im, mask)
+                        #print(im.shape, im_cropped.shape)
+                        #print(mask.shape, mask_cropped.shape)
+                        cv2.imwrite(im_path, cv2.cvtColor(im_cropped, cv2.COLOR_BGR2RGB))
+                        cv2.imwrite(mask_path, cv2.cvtColor(mask_cropped, cv2.COLOR_BGR2RGB))
+                    # Update the progress bar
+                    bar()
 
 
 if __name__ == '__main__':
