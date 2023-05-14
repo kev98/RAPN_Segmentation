7cf1867cd9474bd085166b0117ea53dbe61bb5ce
kev98 <kevinmarchesini@gmail.com>
2023-05-14 10:57:23 +0200

    Split final dataset

--- dataset/split_dataset.py
+++ dataset/split_dataset.py
@@ -11,7 +11,7 @@ import math
 
 def main():
     # Specify the dataset folder
-    source_folder = r"/home/kmarc/workspace/nas_private/Segmentation_Dataset_RAPN/masks"
+    source_folder = r"/home/kmarc/workspace/nas_private/Segmentation_Dataset_RAPN_final_2/masks"
     #source_folder = "/Volumes/ORSI/Kevin/Dataset_RAPN_20procedures/train/masks"
     masks = glob.glob(source_folder + '/*/*.png')
     dir = os.listdir(source_folder)
@@ -25,12 +25,14 @@ def main():
     masks.sort()
 
     # class distribution among the RAPN100 Dataset images, previously computed (to be changed)
-    class_distribution = [30261, 1360, 762, 1874, 11, 61, 784, 117, 217, 5665, 18, 15239, 1500, 536, 7717, 352, 99, 936,
-                          470, 30, 12650, 1, 0, 60, 22938, 4464, 0, 6, 12130, 37, 5109, 8035, 712, 3035, 0, 1031, 1093,
-                          22, 244, 31]
-    tot_images = 30438 # total number of images in the dataset (to be changed)
+    #class_distribution = [30261, 1360, 762, 1874, 11, 61, 784, 117, 217, 5665, 18, 15239, 1500, 536, 7717, 352, 99, 936,
+    #                      470, 30, 12650, 1, 0, 60, 22938, 4464, 0, 6, 12130, 37, 5109, 8035, 712, 3035, 0, 1031, 1093,
+    #                      22, 244, 31]
+    class_distribution = [31871, 1525, 848, 2618, 10, 109, 811, 82, 265, 7496, 821, 14956, 2233, 574, 8069, 35, 80, 980,
+                          500, 31, 12776, 4, 0, 61, 24007, 4210, 349, 17, 12328, 40, 5290, 8287, 1087, 3523, 0, 321]
+    tot_images = 31871 # total number of images in the dataset (to be updated)
     class_distribution = [c/tot_images for c in class_distribution]
-    num_classes = 42
+    num_classes = 36
 
     print(class_distribution)
 
@@ -78,11 +80,23 @@ def main():
      'Suture wire', 'Veriset', 'Vessel Loop', 'Vessel Sealer Extend', 'Echography', 'Da Vinci trocar',
      'Assistant trocar', 'Airseal trocar', 'Foam extruder']
 
+    classes_list = ['Background', 'Bulldog clamp', 'Bulldog wire', 'Cadiere Forceps', 'Da Vinci Obturator',
+                    'Drain', 'Endobag', 'Endobag specimen retriever', 'Endobag wire', 'Fenestrated Bipolar Forceps',
+                    'Endoscope Trocar',
+                    'Force Bipolar', 'Hemostasis', 'Hemolock Clip Applier', 'Hemolock Clip', 'Floseal_Extruder',
+                    'Laparoscopic Clip Applier',
+                    'Laparoscopic Fenestrated Forceps', 'Laparoscopic Needle Driver', 'Laparoscopic Scissors',
+                    'Large Needle Driver',
+                    'Tachoseal_introducer', 'Maryland Bipolar Forceps', 'Metal clip', 'Monopolar Curved Scissors',
+                    'Prograsp Forceps', 'Airseal trocar', 'Assistant trocar', 'Suction', 'Surgical_Glove_Tip',
+                    'Suture needle',
+                    'Suture wire', 'Echography', 'Vessel Loop', 'Vessel Sealer Extend', 'Da Vinci trocar']
+
     # create a Dataframe with the occurencies of each class
     df = pd.DataFrame(distances, index=dir, columns=['Occurencies'])
     print(df)
 
-    df.to_excel("/home/kmarc/workspace/nas_private/Segmentation_Dataset_RAPN/" + 'directory_distance.xlsx')
+    df.to_excel("/home/kmarc/workspace/nas_private/Segmentation_Dataset_RAPN_final_2/" + 'directory_distance.xlsx')
 
 
 if __name__ == '__main__':
